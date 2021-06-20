09e97a19014fc8adc0bb98f98db88c1b090d9271
Victor Edward <victoredward30@gmail.com>
2021-06-20 08:30:59 -0300

    feat: Created the component and the page for delete the user

---
+++ user-register-front/components/delete_user/index.tsx
@@ -0,0 +1,13 @@
+
+
+const DeleteUser = () => {
+    return(
+        <div>
+            <p>Tem certeza que quer deletar sua conta usuário?</p>
+            <button>Confirmar</button>
+            <button>Cancelar</button>
+        </div>
+    )
+}
+
+export default DeleteUser
---
+++ user-register-front/pages/confirm_delete/index.tsx
@@ -0,0 +1,8 @@
+import DeleteUser from "../../components/delete_user"
+
+
+const DeleteUserPage = () => {
+    return <DeleteUser />
+}
+
+export default DeleteUserPage
